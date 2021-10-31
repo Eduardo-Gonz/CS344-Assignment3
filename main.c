54ea24f1b919f54f8a9f6172754698afdf6e3c04
Eduardo-Gonz <erugonz249@gmail.com>
2021-10-30 19:43:06 -0700

    initial commit to create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+
+int main()
+{
+    printf("Program!!");
+
+    return EXIT_SUCCESS;
+}
