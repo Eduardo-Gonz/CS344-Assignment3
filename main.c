4d0dc001b33d918848b22eeacf5fcfb2c9596d3d
Eduardo-Gonz <erugonz249@gmail.com>
2021-11-01 00:02:38 -0700

    Check for comments and blank lines

--- main.c
+++ main.c
@@ -8,11 +8,43 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+int ignoreCmd(char usrCmd []) {
+    char firstChar = usrCmd[0];
+    if(firstChar == '#' || strcmp(usrCmd, "\n") == 0){
+        printf("Sorry, blank lines and comments are ignored\n");
+        return 1;
+    }
+    
+
+    return 0;
+}
+
+void createCmdLine() {
+    char cmd [2049];
+    char args [513];
+    int exit = 0;
+
+    do{
+        printf(": ");
+        fgets(cmd, 2049, stdin);
+        fflush(stdout);
+
+        //check for blank lines or comments
+        if(ignoreCmd(cmd))
+            continue;
+        else
+            exit ++;
+
+        //check for three commands
+        //make a process and call execvp()
+    }while(exit < 1);
+
+}
 
 
 int main()
 {
-    printf("Program!!");
+    createCmdLine();
 
     return EXIT_SUCCESS;
 }
